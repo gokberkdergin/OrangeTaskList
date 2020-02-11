9cf3461b23b7bed22c0652d6a881a6cadcc0ee2f
Gokberk Dergin <46280408+gokberkdergin@users.noreply.github.com>
2020-02-11 14:17:22 +0300

    Add files via upload

---
+++ ToDoList/ToDoList/ToDoCell.swift
@@ -0,0 +1,23 @@
+
+
+import UIKit
+
+//Protcol to pass the cell back to the designated delegate class. The delegate is the ToDoTableViewController class
+@objc protocol ToDoCellDelegate: class {
+    
+    func completeButtonTapped(sender: ToDoCell)
+}
+
+class ToDoCell: UITableViewCell {
+    
+    //Delegate property
+    var delegate: ToDoCellDelegate?
+    
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBAction func isCompleteButtonTapped() {
+        delegate?.completeButtonTapped(sender: self)
+        //This informs the delegate (which is the TableViewController) the button has been tapped, passing self as the parameter
+    }
+}
